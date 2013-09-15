f759fe1d0d2d029182e42970a6d08a6089be01ee
Tyler Lubeck <tyler@tylerlubeck.com>
2013-09-14 23:55:59 -0400

    Switch to Heroku

--- HalliganAvailability/settings.py
+++ HalliganAvailability/settings.py
@@ -156,3 +156,23 @@ LOGGING = {
         },
     }
 }
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config(default='postgres://django_login:***REMOVED***@localhost:5432/django_db')
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
