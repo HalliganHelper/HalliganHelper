99f0ad351b3e111c41ea6f222e637cd548bf1a7c
Tyler Lubeck <tyler@tylerlubeck.com>
2013-09-15 21:06:56 -0400

    Turn off Debug and remove admin

--- HalliganAvailability/settings.py
+++ HalliganAvailability/settings.py
@@ -1,6 +1,6 @@
 # Django settings for HalliganAvailability project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
--- HalliganAvailability/urls.py
+++ HalliganAvailability/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('HalliganComputerAvailability.urls')),
     url(r'^ta/', include('HalliganTAAvailability.urls')),
     url(r'^$', 'HalliganComputerAvailability.views.HomePage'),
