9f7ec93cc932b4fb8aaf4bd832e6ff8a62940f09
Tyler Lubeck <tyler@tylerlubeck.com>
2013-09-17 04:12:36 -0400

    Collect Static Files

--- staticfiles/js/CreateGraph.js
+++ staticfiles/js/CreateGraph.js
@@ -1,13 +1,37 @@
 
 function drawCharts(){
-    $.get('/api/serverInfo', function(data){
-        var ChartData = [];
-        ChartData.push(['Time', 'Number of Users'])
+    $.get('/api/serverInfo?NumDataPoints=10', function(data){
+        for (server in data) {
+            var ChartData = [];
+            ChartData.push(['Time', 'Number of Users'])
 
-        for (update in data['homework']){
-            ChartData.push([update.fields.Updated, update.fields.NumUsers])
+            for (update in data[server]){
+                var date = data[server][update].fields.Updated;
+                var year = date.slice(0,4);
+                var month = date.slice(5,7);
+                var day = date.slice(8,10);
+                var hour = date.slice(11,13);
+                var minute = date.slice(14,16);
+                var second = date.slice(17,19);
+                var milli = date.slice(20,23);
+
+                date = new Date(year, month, day, hour, minute, second, milli);
+                var numUsers = data[server][update].fields.NumUsers;
+                ChartData.push([date.toTimeString().slice(0,5), numUsers])
+            }
+            ChartData = google.visualization.arrayToDataTable(ChartData);
+            var options = {
+                title: server,
+                legend: {position: 'none'},
+                width: 800,
+                height: 500
+            };
+
+            //2013-09-17T05:40:31.515
+
+            var chart = new google.visualization.LineChart(document.getElementById('ChartFor' + server));
+            chart.draw(ChartData, options);
         }
-        console.log(ChartData)
         /*
         for (server in data){
             for (update in server){
