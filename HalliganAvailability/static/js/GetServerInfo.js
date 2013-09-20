6bae812c42935033418af5ea691021006e7ac7c5
Tyler Lubeck <tyler@tylerlubeck.com>
2013-09-20 15:36:10 -0400

    Add refresh button and interval

--- HalliganAvailability/static/js/GetServerInfo.js
+++ HalliganAvailability/static/js/GetServerInfo.js
@@ -6,9 +6,12 @@
  * To change this template use File | Settings | File Templates.
  */
 
+var ListServersInterval = null;
+
 function ListServers() {
     $.get('/api/ServerList', function(data) {
         var DataDiv = $('#ServerContainer');
+        $(DataDiv).empty();
 
         var table = $('<table></table>').addClass('striped rounded');
 
@@ -16,7 +19,17 @@ function ListServers() {
         var row = $('<tr></tr>');
         $(row).append($('<td></td>').text('Server'));
         $(row).append($('<td></td>').text('Users'));
-        $(row).append($('<td></td>').text('Last Updated'));
+
+        var LastTD = $('<td></td>').text('Last Updated');
+        var RefreshButton = $('<i></i>').addClass('icon-arrows-ccw ttip');
+        var UpdateTime = new Date();
+        var RefreshText = $('<span></span>').html("Updates are received from computers every 15 minutes.<br/> This table will refresh automatically every 15 minutes. <br/> This table was last refreshed at " + UpdateTime.toLocaleTimeString())
+        var refreshLink = $('<a href="#"></a>').append(RefreshButton).append(RefreshText).addClass('Refresh hasTooltip');
+        $(refreshLink).click(function() {
+            ListServers();
+        })
+        $(LastTD).append(refreshLink);
+        $(row).append(LastTD)
 
         $(head).append(row);
         var body = $('<tbody></tbody>');
@@ -26,7 +39,6 @@ function ListServers() {
             $(row).append($('<td></td>').text(server.ComputerName));
             $(row).append($('<td></td>').text(server.NumUsers));
             $(row).append($('<td></td>').text(server.LastUpdated));
-
             $(body).append(row);
         }
 
@@ -36,6 +48,8 @@ function ListServers() {
         $(DataDiv).append(table);
 
     })
+    window.clearInterval(ListServersInterval);
+    ListServersInterval = window.setInterval(ListServers, 900000)
 }
 
 $(document).ready(ListServers)
