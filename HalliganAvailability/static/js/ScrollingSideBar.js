1952f47ee51d8c08a4404c801df41e42ff6c5e18
Tyler Lubeck <tyler@tylerlubeck.com>
2014-01-18 19:18:17 -0500

    Remove unnecessary code

--- HalliganAvailability/static/js/ScrollingSideBar.js
+++ HalliganAvailability/static/js/ScrollingSideBar.js
@@ -1,39 +1,6 @@
 var ShowingToTop = false;
 
 $(function(){
-    /*
-    var c11 = $("div").find("[data-target='comp11']")[0],
-        c15 = $("div").find("[data-target='comp15']")[0],
-        c40 = $("div").find("[data-target='comp40']")[0],
-        c105 = $("div").find("[data-target='comp105']")[0];
-
-
-    c11 = $(c11).position().top - 15;
-    c15 = $(c15).position().top - 15;
-    c40 = $(c40).position().top - 15;
-    c105 = $(c105).position().top - 15;
-
-    $(window).scroll(function scrollFunc(){
-        var top = $(window).scrollTop();
-        console.log(top);
-        if(top >= c105){
-            console.log('Looking at 105');
-            $("#sidebar-nav li").removeClass("active");
-        } else if (top >= c40) {
-            $("#sidebar-nav li").removeClass("active");
-            $("#comp40Nav").addClass("active");
-        } else if (top >= c15) {
-            $("#sidebar-nav li").removeClass("active");
-            $("#comp15Nav").addClass("active");
-        } else if (top >= c11){
-            $("#sidebar-nav li").removeClass("active");
-            $("#comp11Nav").addClass("active");
-        }
-
-
-    });
-    */
-
     $("#sidebar-nav li").on('click', function navClicked(){
         var $this = $(this);
         $("#sidebar-nav li").removeClass("active");
