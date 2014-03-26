c420eaa60c6d28227e68594491f09f556b3e5489
Tyler Lubeck <tyler@tylerlubeck.com>
2014-03-26 19:28:02 -0400

    Filter and order correctly

--- HalliganComputerAvailability/api.py
+++ HalliganComputerAvailability/api.py
@@ -6,20 +6,16 @@ class RoomInfoResource(ModelResource):
     cuis = fields.ToManyField('HalliganComputerAvailability.api.CourseUsageInfoResource', 'cuis', full=True)
 
     class Meta:
-        queryset = RoomInfo.objects.all()
+        queryset = RoomInfo.objects.all().order_by('-updateTime')
+        filtering = {
+            'lab': ['exact', ],
+        }
         resource_name = 'roominfo'
         fields = ['lab', 'numReporting', 'num_available', 'num_unavailable',
                   'num_error', 'updateTime']
         allowed_methods = ['get']
         limit = 100
 
-    def obj_get_list(self, bundle, **kwargs):
-        full_query = RoomInfo.objects.all()
-        if 'room' in bundle.request.GET:
-            param = bundle.request.GET['room']
-            full_query = full_query.filter(lab=param)
-   
-        return full_query.order_by('updateTime')
 
 class CourseUsageInfoResource(ModelResource):
     room = fields.ToOneField(RoomInfoResource, 'room')
