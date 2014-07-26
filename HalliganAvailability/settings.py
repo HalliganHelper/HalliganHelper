40d691c1ca50cfe0ffa173225b004400f3c383b1
Tyler Lubeck <tyler@tylerlubeck.com>
2014-07-26 14:48:03 -0400

    weird space

--- HalliganAvailability/settings.py
+++ HalliganAvailability/settings.py
@@ -9,7 +9,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
