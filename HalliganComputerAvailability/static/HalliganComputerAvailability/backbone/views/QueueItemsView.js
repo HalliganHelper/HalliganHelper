495acc1f0e38fe511a264f61735d052f492af29c
Tyler Lubeck <tyler@tylerlubeck.com>
2014-10-17 14:02:59 -0400

    use origin in save urls

--- HalliganComputerAvailability/static/HalliganComputerAvailability/backbone/views/QueueItemsView.js
+++ HalliganComputerAvailability/static/HalliganComputerAvailability/backbone/views/QueueItemsView.js
@@ -100,7 +100,7 @@ app.queueItemsView = Backbone.View.extend({
                 id: objectId,
             });
 
-            newRequest.url = 'https://' + document.location.host + '/api/v2/request/checkout_request';
+            newRequest.url = document.location.origin + '/api/v2/request/checkout_request';
             newRequest.save();
     },
     cancelRequest: function(ev) {
@@ -110,13 +110,11 @@ app.queueItemsView = Backbone.View.extend({
         var newRequest = new app.QueueItem({
             id: objectId,
         });
-        newRequest.url = 'https://' + document.location.host + '/api/v2/request/cancel_request';
+        newRequest.url = document.location.origin + '/api/v2/request/cancel_request';
         newRequest.save({}, {
             success: function cancelSuccess() {
                 _this.removeContainerDiv(objectId);
             },
-            error: function cancelFailure() {
-            }
         });
     },
     resolveRequest: function(ev) {
@@ -127,7 +125,7 @@ app.queueItemsView = Backbone.View.extend({
             id: objectId,
         });
 
-        newRequest.url = 'https://' + document.location.host + '/api/v2/request/resolve_request';
+        newRequest.url = document.location.origin + '/api/v2/request/resolve_request';
         newRequest.save({}, {
             success: function resolveSuccess() {
                 _this.removeContainerDiv(objectId);
