8c03d8fc304cfc65b5a7e751df6f53333e83b89f
Tyler Lubeck <tyler@tylerlubeck.com>
2014-11-14 01:00:18 -0500

    finish removing redundant urls

--- HalliganComputerAvailability/urls.py
+++ HalliganComputerAvailability/urls.py
@@ -2,7 +2,5 @@ __author__ = 'tyler'
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('HalliganComputerAvailability.views',
-                       url(r'^updateServer/(?P<MchID>.+)/(?P<NewStatus>.+)/(?P<num_users>\d+)',
-                           'UpdateServer',
-                           name='UpdateServer'),
+
                        )
