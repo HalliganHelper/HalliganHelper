1308c15f1c31f10d1f63ed4ea95293cc5fc1124d
Tyler Lubeck <tyler@tylerlubeck.com>
2014-11-14 00:05:33 -0500

    Remove old views

--- HalliganComputerAvailability/urls.py
+++ HalliganComputerAvailability/urls.py
@@ -2,20 +2,7 @@ __author__ = 'tyler'
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('HalliganComputerAvailability.views',
-                       url(r'^room/(?P<RmNum>.+)/$',
-                           'SpecificRoom',
-                           name='SpecificRoom'),
-                       url(r'^machine$',
-                           'SpecificMachine',
-                           name='SpecificMachine'),
-                       url(r'^update/(?P<MchID>.+)/(?P<NewStatus>.+)',
-                           'UpdateStatus',
-                           name='UpdateStatus'),
                        url(r'^updateServer/(?P<MchID>.+)/(?P<NewStatus>.+)/(?P<num_users>\d+)',
                            'UpdateServer',
                            name='UpdateServer'),
-                       url(r'^updateLab', 'UpdateLab', name='UpdateLab'),
-                       url(r'^UpdateAllStatus',
-                           'UpdateAllStatus',
-                           name='UpdateAllStatus'),
                        )
