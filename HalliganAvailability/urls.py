9050d033637b21bfd43a9ce60b7fd39377a21e42
Tyler Lubeck <tyler@tylerlubeck.com>
2015-04-05 12:28:01 -0400

    Only show static file urls when DEBUG is on

--- HalliganAvailability/urls.py
+++ HalliganAvailability/urls.py
@@ -45,6 +45,6 @@ urlpatterns = patterns('',
                        url(r'^$', 'HalliganComputerAvailability.views.ModularHomePage', name='ModularHomePage'),
                        )
 
-urlpatterns += staticfiles_urlpatterns()
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
