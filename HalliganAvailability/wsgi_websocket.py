0c19309d6cb8dbfa4e7c94c0f6ff9a1262f02b7a
Tyler Lubeck <tyler@tylerlubeck.com>
2015-10-04 19:09:47 -0700

    Add the websocket wsgi handler

---
+++ HalliganAvailability/wsgi_websocket.py
@@ -0,0 +1,9 @@
+import os
+import gevent.socket
+import redis.connection
+
+redis.connection.socket = gevent.socket
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HalliganAvailability.settings")
+
+from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
+application = uWSGIWebsocketServer()
