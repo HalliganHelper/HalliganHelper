5b9d99d7819d5b4038385867448563701d9875fa
Tyler Lubeck <tyler@tylerlubeck.com>
2015-12-17 00:16:36 -0800

    unregister everything

--- tas/admin.py
+++ tas/admin.py
@@ -3,7 +3,7 @@ from .custom_user_admin import CustomUserAdmin
 from .models import Student, OfficeHour, Course, Request
 
 
-admin.site.register(Student)
-admin.site.register(OfficeHour)
-admin.site.register(Course)
-admin.site.register(Request)
+# admin.site.register(Student)
+# admin.site.register(OfficeHour)
+# admin.site.register(Course)
+# admin.site.register(Request)
