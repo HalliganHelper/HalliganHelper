02b8931c0d55636e7ce6a9cf63eba442affbae7e
Thomas Hebb <tommyhebb@gmail.com>
2016-09-06 02:53:14 -0400

    Add import change that should have been in the last commit
    This is why you don't make changes at 3am...

--- tas/management/commands/NewTAs.py
+++ tas/management/commands/NewTAs.py
@@ -1,6 +1,6 @@
 __author__ = 'tyler'
 from django.core.management.base import BaseCommand, CommandError
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from tas.models import Course, TA
 import requests
 from django.core.mail import EmailMultiAlternatives
